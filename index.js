1594fa80e28cf44817bac86a0d127b7a99d6e35f
WILMAR ROJAS <wilmarandresrojasvillafane@gmail.com>
2022-10-31 22:09:44 -0600

    Estrucuturar  Carrito en EJS

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = requeire('express')
+const express = require('express')
 const app = express();
 const port = 3000;
 
 //motor de plantillas 
 app.set('view engine', 'ejs');
-app.set('views', __dirname +  '/views');
+app.set('views', __dirname +  '/js');
+app.set('views', __dirname +  '/view');
+
+app.use("/public", express.static("public"));
+
+app.get('/', (req, res) => {    
+    res.render("carrito");
+  })
+
+
+  app.get('/carritoComprado', (req, res) => {
+    res.render("carritoComprado")
+  })
+
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
---
+++ public/js/main.js
@@ -0,0 +1,99 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
+var productos = [];
+
+function ClickBuscar(){
+
+    let busqueda = document.getElementById("busqueda").value;
+
+    $.get("https://api.mercadolibre.com/sites/MCO/search?q="+busqueda, function(data){
+    data.results.forEach(producto => {
+
+        let datosProducto = {
+            imagen: producto.thumbnail,
+            titulo: producto.title,
+            precio: producto.price,
+            id: producto.id,
+            cantidad: 1,
+        };
+
+        productos.push(datosProducto);
+
+
+        let row = `<div class="col-12 col-md-6 col-lg-4">
+                    <div class="card m-2" >
+                     <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${producto.thumbnail}" class="img-fluid rounded-start" alt="Image Product" style="padding: 10px;" >
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h7 class="card-title title-elipsis">${producto.title}</h7>
+                                <p class="card-text"><small class="text-success" class="text-muted">$ ${(producto.price).toLocaleString('en-US')}</small></p>
+                                <input type="number" id="cantidad_${producto.id}" min="0" max="15" value="${datosProducto.cantidad}" style="width: 40px;">
+                                <button class="btn btn-success" onclick ="Agregar('${producto.id}');" type="button" style="float:right; margin-bottom: 10px; text-align: end;">Agregar</button>
+                            </div>
+                        </div>
+                     </div>
+                    </div> 
+                   </div>      
+                   `
+
+       document.getElementById("listar").innerHTML += row;
+  
+        });
+    })
+
+}
+
+function Agregar(id) {
+    
+   productos.forEach(p => {
+
+    if(p.id == id){
+
+        p.cantidad = document.getElementById("cantidad_"+p.id).value;   
+        
+        AgregarProductoLocalStorge(p);
+    }
+
+   });
+
+}
+
+function AgregarProductoLocalStorge(producto){
+  
+    let localCarrito = localStorage.getItem("carrito");
+
+    if(localCarrito){
+        let datosCarrito = JSON.parse(localCarrito); //JSON.parse => sirve para que de un string lo convierta a un objeto
+       
+        datosCarrito.push(producto);
+
+        localStorage.setItem("carrito", JSON.stringify(datosCarrito));
+    }else{
+        localStorage.setItem("carrito", JSON.stringify([producto])); 
+    }
+
+
+    Toast.fire({
+        icon: 'success',
+        title: 'Agregado con éxito'
+    })
+}
+
+
+
+
+
+
